25311fb421c991f14177a6fadc2273f64d51ef55
Baidaidai_GFWD <1302064396@qq.com>
2026-01-03 16:48:32 +0800

    feat(HowToDevelopRootlessStorePlugin): Initialize the HowToDevelopRootlessStorePlugin card

---
+++ app/src/main/java/com/baidaidai/rootless_store/components/stratScreen/components/HowToDevelopRootlessStorePlugin.kt
@@ -0,0 +1,61 @@
+package com.baidaidai.rootless_store.components.stratScreen.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.baidaidai.rootless_store.R
+
+@Composable
+fun HowToDevelopRootlessStorePlugin(){
+    Card(
+        modifier = Modifier
+            .fillMaxWidth(),
+        elevation = CardDefaults.cardElevation()
+    ){
+        Column(
+            modifier = Modifier
+                .padding(30.dp)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Icon(
+                    painter = painterResource(R.drawable.outline_construction_24),
+                    contentDescription = "Develop Icon",
+                    modifier = Modifier
+                        .size(30.dp)
+                )
+                Spacer(
+                    modifier = Modifier
+                        .width(20.dp)
+                )
+                Column{
+                    Text(
+                        text = "Learn Rootless Store",
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                    Text(
+                        text = "Learn how to develop RootlessStore plugins",
+                        style = MaterialTheme.typography.labelSmall
+                    )
+                }
+            }
+        }
+    }
+}
