c6e5531c7e37c6a2c78fed5d4153de9152411248
Baidaidai_GFWD <1302064396@qq.com>
2026-01-03 16:46:56 +0800

    feat(NecessaryComponents): Initialize the NecessaryComponents

---
+++ app/src/main/java/com/baidaidai/rootless_store/components/stratScreen/components/NecessaryComponents.kt
@@ -0,0 +1,70 @@
+package com.baidaidai.rootless_store.components.stratScreen.components
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import com.baidaidai.rootless_store.R
+
+object NecessaryComponents {
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun StartScreenTopAppBar(){
+        TopAppBar(
+            title = {
+                Text("Rootless Store")
+            }
+        )
+    }
+
+    @Composable
+    fun StartScreenNavigationBar(){
+        NavigationBar {
+            NavigationBarItem(
+                selected = true,
+                onClick = {},
+                icon = {
+                    Icon(
+                        Icons.Outlined.Home,
+                        contentDescription = "Home"
+                    )
+                },
+                label = {
+                    Text("Home")
+                }
+            )
+            NavigationBarItem(
+                selected = false,
+                onClick = {},
+                icon = {
+                    Icon(
+                        painter = painterResource(R.drawable.outline_extension_24),
+                        contentDescription = "Plugins"
+                    )
+                },
+                label = {
+                    Text("Plugins")
+                }
+            )
+            NavigationBarItem(
+                selected = false,
+                onClick = {},
+                icon = {
+                    Icon(
+                        painter = painterResource(R.drawable.outline_list_alt_24),
+                        contentDescription = "Sources"
+                    )
+                },
+                label = {
+                    Text("Sources")
+                }
+            )
+        }
+    }
+}
