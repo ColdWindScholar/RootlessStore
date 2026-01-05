421ee7858162a6c0747d8447fd97b4efe9139ff7
Baidaidai_GFWD <1302064396@qq.com>
2026-01-05 18:35:36 +0800

    chore: Import some libs

--- app/src/main/java/com/baidaidai/rootless_store/model/RootlessStoreStratScreenViewModel.kt
+++ app/src/main/java/com/baidaidai/rootless_store/model/RootlessStoreStratScreenViewModel.kt
@@ -1,19 +1,27 @@
 package com.baidaidai.rootless_store.model
 
 import android.content.Context
+import android.util.Log
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelStore
+import androidx.lifecycle.viewModelScope
+import com.baidaidai.rootless_store.data.hosterstatus.impl.RAMStatusGatewayImpl
 import com.baidaidai.rootless_store.data.hosterstatus.impl.StorageGatewayImpl
+import com.baidaidai.rootless_store.domain.hosterstatus.model.RAMStatus
 import com.baidaidai.rootless_store.domain.hosterstatus.model.StorageStatus
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 class RootlessStoreStratScreenViewModel: ViewModel() {
     private var context: Context? = null
 
     private val _storageStatus: MutableStateFlow<StorageStatus> = MutableStateFlow(getStorageStatus())
+    private val _ramStatus: MutableStateFlow<RAMStatus> = MutableStateFlow(getRAMStatus())
     val storageStatus: StateFlow<StorageStatus> = _storageStatus.asStateFlow()
 
 
