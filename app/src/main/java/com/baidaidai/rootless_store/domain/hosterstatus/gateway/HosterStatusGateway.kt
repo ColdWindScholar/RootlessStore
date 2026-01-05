4cbcc533dbafab6b03e8e0947b968e31c20576d9
Baidaidai_GFWD <1302064396@qq.com>
2026-01-05 16:59:35 +0800

    fix: Remove the wrong code

--- app/src/main/java/com/baidaidai/rootless_store/domain/hosterstatus/gateway/HosterStatusGateway.kt
+++ app/src/main/java/com/baidaidai/rootless_store/domain/hosterstatus/gateway/HosterStatusGateway.kt
@@ -5,5 +5,4 @@ import com.baidaidai.rootless_store.domain.hosterstatus.model.StorageStatus
 
 interface StorageGetAway {
     fun getStorageStatus(): StorageStatus
-    fun getRAMStatus(): RAMStatus
 }
