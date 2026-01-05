67d7c84ec8fa0d68f2250b9a4dc36527c901b4f9
Baidaidai_GFWD <1302064396@qq.com>
2026-01-05 16:29:28 +0800

    chore: Import the mutableDoubleStateOf support

--- app/src/main/java/com/baidaidai/rootless_store/components/stratScreen/components/HosterStatusProgressRow.kt
+++ app/src/main/java/com/baidaidai/rootless_store/components/stratScreen/components/HosterStatusProgressRow.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableDoubleStateOf
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.ui.graphics.Color
