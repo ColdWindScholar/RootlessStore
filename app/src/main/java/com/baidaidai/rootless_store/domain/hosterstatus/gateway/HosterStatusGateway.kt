ca8049955f037183018eee4ddebe1e26433289bc
Baidaidai_GFWD <1302064396@qq.com>
2026-01-05 16:51:03 +0800

    feat: Preparing for add RAMStatus

--- app/src/main/java/com/baidaidai/rootless_store/domain/hosterstatus/gateway/HosterStatusGateway.kt
+++ app/src/main/java/com/baidaidai/rootless_store/domain/hosterstatus/gateway/HosterStatusGateway.kt
@@ -1,7 +1,9 @@
 package com.baidaidai.rootless_store.domain.hosterstatus.gateway
 
+import com.baidaidai.rootless_store.domain.hosterstatus.model.RAMStatus
 import com.baidaidai.rootless_store.domain.hosterstatus.model.StorageStatus
 
 interface StorageGetAway {
     fun getStorageStatus(): StorageStatus
+    fun getRAMStatus(): RAMStatus
 }
