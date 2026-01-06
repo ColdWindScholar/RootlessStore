6d4306b9122f9d5ea7d7b4e0f8b1a19851ebd142
Baidaidai_GFWD <1302064396@qq.com>
2026-01-06 11:13:33 +0800

    refactor: Remove the useless files

--- app/src/main/java/com/baidaidai/rootless_store/components/stratScreen/model/RAMStatus.kt
+++
@@ -1,6 +0,0 @@
-package com.baidaidai.rootless_store.components.stratScreen.model
-
-data class RAMStatus(
-    val totalRAM: Float,
-    val usedRAM: Float
-)
--- app/src/main/java/com/baidaidai/rootless_store/components/stratScreen/model/StorageStatus.kt
+++
@@ -1,6 +0,0 @@
-package com.baidaidai.rootless_store.components.stratScreen.model
-
-data class StorageStatus(
-    val totalStorage: Float,
-    val usedStorage: Float
-)
