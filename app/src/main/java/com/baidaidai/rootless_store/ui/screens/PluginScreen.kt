fd4db5e96c125efeaeef07c97f1776beba814c62
Baidaidai_GFWD <1302064396@qq.com>
2026-01-06 12:17:13 +0800

    chore: Import the necessary libs

--- app/src/main/java/com/baidaidai/rootless_store/ui/screens/PluginScreen.kt
+++ app/src/main/java/com/baidaidai/rootless_store/ui/screens/PluginScreen.kt
@@ -1,10 +1,17 @@
 package com.baidaidai.rootless_store.ui.screens
 
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.PreviewLightDark
 import com.baidaidai.rootless_store.domain.pluginManiFest.model.PluginManiFest
+import com.baidaidai.rootless_store.ui.theme.RootlessStoreTheme
 
 @Composable
 fun RootlessStorePluginScreenContainer(){
