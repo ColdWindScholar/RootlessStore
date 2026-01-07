fc8f15c35a1748fee32adf741015231ea71d73f0
Baidaidai_GFWD <1302064396@qq.com>
2026-01-07 21:22:26 +0800

    feat(HomeScreen): Extract the component and initialize it

---
+++ app/src/main/java/com/baidaidai/rootless_store/ui/screens/HomeScreen.kt
@@ -0,0 +1,46 @@
+package com.baidaidai.rootless_store.ui.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.baidaidai.rootless_store.components.stratScreen.components.HowToDevelopRootlessStorePlugin
+import com.baidaidai.rootless_store.components.stratScreen.components.RootLessStoreVersionCheckerContainer
+import com.baidaidai.rootless_store.components.stratScreen.components.RootlessStoreHosterStatusBoard
+import com.baidaidai.rootless_store.domain.hosterstatus.model.RootlessStoreHosterStatus
+
+@Composable
+fun HomeScreen(
+    contentPadding: PaddingValues,
+    rootlessStoreHosterStatus: RootlessStoreHosterStatus
+){
+    Column(
+        modifier = Modifier
+            .padding(contentPadding)
+            .padding(horizontal = 15.dp)
+            .padding(top = 15.dp)
+    ) {
+        /* Version */
+        RootLessStoreVersionCheckerContainer()
+        Spacer(
+            modifier = Modifier
+                .height(20.dp)
+        )
+
+        /* Hoster Status */
+        RootlessStoreHosterStatusBoard(
+            hosterStatus = rootlessStoreHosterStatus
+        )
+        Spacer(
+            modifier = Modifier
+                .height(20.dp)
+        )
+
+        /* How to Make Plugin */
+        HowToDevelopRootlessStorePlugin()
+    }
+}
