805f57f0372ea635686f41d1f55e075195aeab56
Baidaidai_GFWD <1302064396@qq.com>
2026-01-08 18:40:51 +0800

    chore: Import the necessary libs

--- app/src/main/java/com/baidaidai/rootless_store/MainActivity.kt
+++ app/src/main/java/com/baidaidai/rootless_store/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.baidaidai.rootless_store
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.ui.platform.LocalContext
 import com.baidaidai.rootless_store.ui.screens.RootlessStoreStartScreenContainer
 import com.baidaidai.rootless_store.ui.theme.*
 class MainActivity : ComponentActivity(){
