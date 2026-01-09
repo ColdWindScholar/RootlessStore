9ecab1acd3cf56608c799f19dc1119c2d2587e05
Baidaidai_GFWD <1302064396@qq.com>
2026-01-09 19:47:55 +0800

    refactor(hosterstatus): remove storage gateway interface

--- app/src/main/java/com/baidaidai/rootless_store/domain/hosterstatus/gateway/HosterStatusGateway.kt
+++
@@ -1,8 +0,0 @@
-package com.baidaidai.rootless_store.domain.hosterstatus.gateway
-
-import com.baidaidai.rootless_store.domain.hosterstatus.model.RAMStatus
-import com.baidaidai.rootless_store.domain.hosterstatus.model.StorageStatus
-
-interface StorageGetAway {
-    fun getStorageStatus(): StorageStatus
-}
