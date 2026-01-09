51a5e14db5cc163be627070df681935f1870fc69
Baidaidai_GFWD <1302064396@qq.com>
2026-01-09 19:46:32 +0800

    refactor(storage): rename gateway implementation and interface for clarity

--- app/src/main/java/com/baidaidai/rootless_store/data/hosterstatus/impl/StorageStatusGatewayImpl.kt
+++ app/src/main/java/com/baidaidai/rootless_store/data/hosterstatus/impl/StorageStatusGatewayImpl.kt
@@ -2,12 +2,12 @@ package com.baidaidai.rootless_store.data.hosterstatus.impl
 
 import android.content.Context
 import com.baidaidai.rootless_store.data.hosterstatus.storage.StorageInfoReader
-import com.baidaidai.rootless_store.domain.hosterstatus.gateway.StorageGetAway
+import com.baidaidai.rootless_store.domain.hosterstatus.gateway.StorageStatusGetAway
 import com.baidaidai.rootless_store.domain.hosterstatus.model.StorageStatus
 
 class StorageGatewayImpl(
     private val context: Context
-) : StorageGetAway {
+) : StorageStatusGetAway {
     override fun getStorageStatus(): StorageStatus {
         val reader = StorageInfoReader(appContext = context)
         val usedStorage = reader.getUsedStorage()
