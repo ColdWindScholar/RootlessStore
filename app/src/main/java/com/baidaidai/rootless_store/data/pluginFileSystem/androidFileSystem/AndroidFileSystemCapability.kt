dd2063c2d8983ace5e53cb9237e6424966d8aa8d
Baidaidai_GFWD <1302064396@qq.com>
2026-01-09 20:37:54 +0800

    feat(file-system): Preserve file name when copying from URI

--- app/src/main/java/com/baidaidai/rootless_store/data/pluginFileSystem/androidFileSystem/AndroidFileSystemCapability.kt
+++ app/src/main/java/com/baidaidai/rootless_store/data/pluginFileSystem/androidFileSystem/AndroidFileSystemCapability.kt
@@ -3,6 +3,7 @@ package com.baidaidai.rootless_store.data.pluginFileSystem.androidFileSystem
 import android.content.Context
 import android.net.Uri
 import android.util.Log
+import androidx.documentfile.provider.DocumentFile
 import java.io.File
 import java.io.FileOutputStream
 
@@ -19,16 +20,24 @@ class AndroidFileSystemCapability(
         }
     }
 
-    fun createOneVoidFile(destination: File): Boolean{
-        Log.d("createOneFile",destination.createNewFile().toString())
-        return destination.createNewFile()
+    fun createOneVoidFile(destination: File,fileName: String): Boolean{
+        val result = File(destination,fileName).createNewFile()  // 创建了文件，而非单纯路径
+        Log.d("createOneFile",result.toString())
+        return result
+    }
+
+    fun getFileNames(originalFileUri: Uri): String{
+        val name = DocumentFile.fromSingleUri(context, originalFileUri)!!.name
+        return name!!
     }
 
     fun copyFile(originFileURI: Uri, destination: File): Boolean {
-        createOneVoidFile(destination)
+        val fileName = getFileNames(originFileURI)
+        createOneVoidFile(destination,fileName)
+        val operationFile = File(destination,fileName)
         return try {
             context.contentResolver.openInputStream(originFileURI).use { input ->
-                FileOutputStream(destination).use { output ->
+                FileOutputStream(operationFile).use { output ->
                     Log.d("copyFile","e")
                     input!!.copyTo(output)
                 }
@@ -41,10 +50,6 @@ class AndroidFileSystemCapability(
             false
         }
     }
-
-//    fun useIntentOpenAndroidSAFAndReturnUri(): Uri{
-//        val originalIntent = Intent(Intent.ACTION_OPEN_DOCUMENT)
-//    }
 
     private fun confirmPathExists(path: String): Boolean{
         Log.d("confirmPathExists",File(context.getExternalFilesDir(null),path.toString()).exists().toString())
