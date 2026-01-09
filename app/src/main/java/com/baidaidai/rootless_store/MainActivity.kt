0338ff13d9cfa828bc84a21cc39a758d97fee590
Baidaidai_GFWD <1302064396@qq.com>
2026-01-09 19:46:56 +0800

    feat(plugin): Add document picker for plugin installation

--- app/src/main/java/com/baidaidai/rootless_store/MainActivity.kt
+++ app/src/main/java/com/baidaidai/rootless_store/MainActivity.kt
@@ -5,15 +5,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.ui.platform.LocalContext
 import com.baidaidai.rootless_store.ui.screens.RootlessStoreStartScreenContainer
 import com.baidaidai.rootless_store.ui.theme.*
+import android.net.Uri
+import androidx.activity.compose.ManagedActivityResultLauncher
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.compose.runtime.remember
+import com.baidaidai.rootless_store.data.pluginFileSystem.impl.PluginFileSystemGatewayImpl
 
 val RootLessStoreLocalContext = compositionLocalOf<Context>{
     error("No Context Provide")
+}
+val OpenDocumentLauncher = compositionLocalOf<ManagedActivityResultLauncher<Array<String>, Uri?>>{
+    error("No ActivityResultLauncher Provide")
 }
 class MainActivity : ComponentActivity(){
     @OptIn(ExperimentalMaterial3ExpressiveApi::class)
@@ -21,9 +30,22 @@ class MainActivity : ComponentActivity(){
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val context = LocalContext.current
+
+            val pluginFileSystemImpl = remember(context) {
+                PluginFileSystemGatewayImpl(context)
+            }
+
+            val openDocumentLauncher = rememberLauncherForActivityResult(
+                contract = ActivityResultContracts.OpenDocument()
+            ) { uri: Uri? ->
+                uri?.let { pluginFileSystemImpl.installPlugin(it) }
+            }
+
             RootlessStoreTheme {
                 CompositionLocalProvider(
-                    RootLessStoreLocalContext provides LocalContext.current
+                    RootLessStoreLocalContext provides context,
+                    OpenDocumentLauncher provides openDocumentLauncher
                 ) {
                     RootlessStoreStartScreenContainer()
                 }
