2c159846b593a97299bf8bf4c6bc4961da203a23
Baidaidai_GFWD <1302064396@qq.com>
2026-01-23 20:46:56 +0800

    style: Delete the useless libs

--- app/src/main/java/com/baidaidai/rootless_store/MainActivity.kt
+++ app/src/main/java/com/baidaidai/rootless_store/MainActivity.kt
@@ -5,17 +5,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.ui.platform.LocalContext
 import com.baidaidai.rootless_store.ui.screens.RootlessStoreStartScreenContainer
 import com.baidaidai.rootless_store.ui.theme.*
-import android.net.Uri
-import androidx.activity.compose.ManagedActivityResultLauncher
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.compose.runtime.remember
 import com.baidaidai.rootless_store.data.pluginFileSystem.impl.PluginFileSystemGatewayImpl
 
 val RootLessStoreLocalContext = compositionLocalOf<Context>{
