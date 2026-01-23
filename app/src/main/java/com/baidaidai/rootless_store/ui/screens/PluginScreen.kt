a4d7d8db28e804d384bffc24b2d43eb273408a2e
Baidaidai_GFWD <1302064396@qq.com>
2026-01-23 20:59:33 +0800

    chore: Remove the useless libs & append the necessary libs

--- app/src/main/java/com/baidaidai/rootless_store/ui/screens/PluginScreen.kt
+++ app/src/main/java/com/baidaidai/rootless_store/ui/screens/PluginScreen.kt
@@ -6,15 +6,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
 import com.baidaidai.rootless_store.components.pluginsScreen.PluginInfosContainer
-import com.baidaidai.rootless_store.domain.pluginManiFest.model.PluginManiFest
-import com.baidaidai.rootless_store.ui.theme.RootlessStoreTheme
+import com.baidaidai.rootless_store.ui.model.RootLessStorePluginScreenViewModel
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
 fun RootlessStorePluginScreenContainer(
