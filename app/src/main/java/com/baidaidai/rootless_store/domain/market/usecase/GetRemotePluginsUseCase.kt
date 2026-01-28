48243258e551a578fdc03e524e1388bcc3124e13
Baidaidai_GFWD <1302064396@qq.com>
2026-01-28 11:50:49 +0800

    refactor: Rename the PluginMarketRepositoryGatewayImpl -> PluginMarketRepositoryImpl

--- app/src/main/java/com/baidaidai/rootless_store/domain/market/usecase/GetRemotePluginsUseCase.kt
+++ app/src/main/java/com/baidaidai/rootless_store/domain/market/usecase/GetRemotePluginsUseCase.kt
@@ -2,12 +2,12 @@ package com.baidaidai.rootless_store.domain.market.usecase
 
 import androidx.paging.PagingData
 import com.baidaidai.rootless_store.data.market.remote.dto.PluginItemDto
-import com.baidaidai.rootless_store.data.market.repository.PluginMarketRepositoryGatewayImpl
+import com.baidaidai.rootless_store.data.market.repository.PluginMarketRepositoryImpl
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class GetRemotePluginsUseCase @Inject constructor(
-    private val repository: PluginMarketRepositoryGatewayImpl
+    private val repository: PluginMarketRepositoryImpl
 ) {
     operator fun invoke(): Flow<PagingData<PluginItemDto>> {
        return repository.getPlugins()
