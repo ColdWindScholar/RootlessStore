361038d2ca503340c1d0d4479d54318c52a4f36f
Baidaidai_GFWD <1302064396@qq.com>
2026-01-28 23:13:23 +0800

    refactor: Change return type intro PluginManifestRemote

--- app/src/main/java/com/baidaidai/rootless_store/domain/market/repository/PluginMarketRepository.kt
+++ app/src/main/java/com/baidaidai/rootless_store/domain/market/repository/PluginMarketRepository.kt
@@ -5,5 +5,5 @@ import com.baidaidai.rootless_store.domain.plugin.manifest.PluginManifestRemote
 import kotlinx.coroutines.flow.Flow
 
 interface PluginMarketRepository {
-    fun getPlugins(): Flow<PagingData<PluginItemDto>>
+    fun getPlugins(): Flow<PagingData<PluginManifestRemote>>
 }
