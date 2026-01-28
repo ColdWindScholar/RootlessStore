c397b80086ce7d5c89d001daef0a0290e07e51c8
Baidaidai_GFWD <1302064396@qq.com>
2026-01-28 21:09:42 +0800

    refactor: Change PluginManifestLocal.kt -> PluginManifestRemote.kt

--- app/src/main/java/com/baidaidai/rootless_store/domain/market/usecase/GetRemotePluginsUseCase.kt
+++ app/src/main/java/com/baidaidai/rootless_store/domain/market/usecase/GetRemotePluginsUseCase.kt
@@ -1,15 +1,15 @@
 package com.baidaidai.rootless_store.domain.market.usecase
 
 import androidx.paging.PagingData
-import com.baidaidai.rootless_store.data.market.remote.dto.PluginItemDto
 import com.baidaidai.rootless_store.data.market.repository.PluginMarketRepositoryImpl
+import com.baidaidai.rootless_store.domain.plugin.manifest.PluginManifestRemote
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class GetRemotePluginsUseCase @Inject constructor(
     private val repository: PluginMarketRepositoryImpl
 ) {
-    operator fun invoke(): Flow<PagingData<PluginItemDto>> {
+    operator fun invoke(): Flow<PagingData<PluginManifestRemote>> {
        return repository.getPlugins()
     }
 }
--- app/src/main/java/com/baidaidai/rootless_store/domain/plugin/usecase/GetWholeManifestUseCase.kt
+++ app/src/main/java/com/baidaidai/rootless_store/domain/plugin/usecase/GetWholeManifestUseCase.kt
@@ -1,13 +1,13 @@
 package com.baidaidai.rootless_store.domain.plugin.usecase
 
 import com.baidaidai.rootless_store.data.plugin.repository.PluginInfoRepositoryImpl
-import com.baidaidai.rootless_store.domain.plugin.model.PluginManifestLocal
+import com.baidaidai.rootless_store.domain.plugin.manifest.PluginManifestRoom
 import javax.inject.Inject
 
 class GetWholePluginInfoUseCase @Inject constructor(
     private val repositoryImpl: PluginInfoRepositoryImpl
 ) {
-    suspend operator fun invoke(): List<PluginManifestLocal> {
+    suspend operator fun invoke(): List<PluginManifestRoom> {
         val result = repositoryImpl.getWholePluginInfo()
 
         if (result.isNullOrEmpty()){
