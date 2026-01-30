e29cfacc6c06f4489612af2f30307d7d7a9c745e
Baidaidai_GFWD <1302064396@qq.com>
2026-01-30 17:28:50 +0800

    feat(overload): Copy PluginInfoContainer logic, use from Switch to Button

--- app/src/main/java/com/baidaidai/rootless_store/components/pluginsScreen/PluginInfoContainer.kt
+++ app/src/main/java/com/baidaidai/rootless_store/components/pluginsScreen/PluginInfoContainer.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.HorizontalDivider
@@ -99,7 +100,65 @@ fun PluginInfoContainer(
     modifier: Modifier = Modifier,
     onClick: ()-> Unit
 ){
-
+    Card(
+        modifier = modifier
+            .fillMaxWidth(),
+        elevation = CardDefaults.cardElevation(),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceContainer
+        )
+    ){
+        Column(
+            modifier = Modifier
+                .padding(24.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Icon(
+                    painter = painterResource(R.drawable.outline_extension_24),
+                    contentDescription = "Plugin Icon",
+                    modifier = Modifier
+                        .size(24.dp)
+                )
+                Spacer(
+                    modifier = Modifier
+                        .width(12.dp)
+                )
+                Column(
+                    modifier = Modifier
+                        .weight(1f)
+                ){
+                    Text(
+                        text = pluginManifest.pluginRenderingName,
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                    Text(
+                        text = "Version: ${pluginManifest.installedVersion}",
+                        style = MaterialTheme.typography.labelMedium
+                    )
+                }
+                Button(
+                    onClick = onClick
+                ){
+                    Text("Install")
+                }
+            }
+            HorizontalDivider()
+            Column(
+                verticalArrangement = Arrangement.spacedBy(6.dp)
+            ) {
+                PluginInfoRow(label = "Author", value = pluginManifest.author)
+                PluginInfoRow(
+                    label = "Required",
+                    value = pluginManifest.requiredEnvironment.toString()
+                )
+            }
+        }
+    }
 }
 
 /**
