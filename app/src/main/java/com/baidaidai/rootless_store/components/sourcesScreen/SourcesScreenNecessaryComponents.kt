7510928c97c83fa4b6e1c1471caeb676c638e7cc
Baidaidai_GFWD <1302064396@qq.com>
2026-01-30 13:12:49 +0800

    feat: Create the SourcesScreenNecessaryComponents.kt

---
+++ app/src/main/java/com/baidaidai/rootless_store/components/sourcesScreen/SourcesScreenNecessaryComponents.kt
@@ -0,0 +1,47 @@
+package com.baidaidai.rootless_store.components.sourcesScreen
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LargeFlexibleTopAppBar
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import com.baidaidai.rootless_store.R
+
+object SourcesScreenNecessaryComponents {
+    @OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3ExpressiveApi::class)
+    @Composable
+    fun SourcesScreenTopAppBar(
+        textButtonOnClick:()-> Unit = {},
+        iconButtonOnClick:()-> Unit = {}
+    ){
+        LargeFlexibleTopAppBar(
+            title = {
+                Text("Plugin")
+            },
+            subtitle = {
+                Text(text = "Instead 1 plugin")
+            },
+            navigationIcon = {
+                TextButton(
+                    onClick = textButtonOnClick
+                ) {
+                    Text("Edit")
+                }
+            },
+            actions = {
+                IconButton(
+                    onClick = iconButtonOnClick
+                ) {
+                    Icon(
+                        painter = painterResource(R.drawable.material_symbols_24px),
+                        contentDescription = "Add"
+                    )
+                }
+            }
+        )
+    }
+}
