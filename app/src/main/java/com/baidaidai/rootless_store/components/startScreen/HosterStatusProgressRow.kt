fa164919789fd3ff7d08109616a51ebbeeed80e0
Baidaidai_GFWD <1302064396@qq.com>
2026-01-30 13:15:43 +0800

    chore: Modify package name, Remove useless libs

--- app/src/main/java/com/baidaidai/rootless_store/components/startScreen/HosterStatusProgressRow.kt
+++ app/src/main/java/com/baidaidai/rootless_store/components/startScreen/HosterStatusProgressRow.kt
@@ -1,4 +1,4 @@
-package com.baidaidai.rootless_store.components.stratScreen.components
+package com.baidaidai.rootless_store.components.startScreen
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
@@ -9,14 +9,8 @@ import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableDoubleStateOf
-import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.PreviewLightDark
