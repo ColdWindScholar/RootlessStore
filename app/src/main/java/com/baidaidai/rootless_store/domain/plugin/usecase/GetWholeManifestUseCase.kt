56aea69b8024a5c9676fef743a22a2b8e8bea7b7
Baidaidai_GFWD <1302064396@qq.com>
2026-02-02 14:59:01 +0800

    refactor(Flow): Change return type from List to Flow<List>

--- app/src/main/java/com/baidaidai/rootless_store/domain/plugin/usecase/GetWholeManifestUseCase.kt
+++ app/src/main/java/com/baidaidai/rootless_store/domain/plugin/usecase/GetWholeManifestUseCase.kt
@@ -2,12 +2,14 @@ package com.baidaidai.rootless_store.domain.plugin.usecase
 
 import com.baidaidai.rootless_store.data.plugin.repository.PluginInfoRepositoryImpl
 import com.baidaidai.rootless_store.domain.plugin.manifest.PluginManifestRoom
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class GetWholePluginInfoUseCase @Inject constructor(
     private val repositoryImpl: PluginInfoRepositoryImpl
 ) {
-    suspend operator fun invoke(): List<PluginManifestRoom> {
+    operator fun invoke(): Flow<List<PluginManifestRoom>> {
         val result = repositoryImpl.getWholePluginInfo()
 
         if (result.isNullOrEmpty()){
