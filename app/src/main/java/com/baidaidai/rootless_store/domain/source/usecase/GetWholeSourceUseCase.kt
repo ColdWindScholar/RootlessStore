7788fb338743ba6482d8b80b39b9dd7842297147
Baidaidai_GFWD <1302064396@qq.com>
2026-02-03 16:27:58 +0800

    refactor: Change from PluginSource to PluginSourceLocal

--- app/src/main/java/com/baidaidai/rootless_store/domain/source/usecase/GetWholeSourceUseCase.kt
+++ app/src/main/java/com/baidaidai/rootless_store/domain/source/usecase/GetWholeSourceUseCase.kt
@@ -1,7 +1,7 @@
 package com.baidaidai.rootless_store.domain.source.usecase
 
 import com.baidaidai.rootless_store.data.source.repository.PluginSourceRepositoryImpl
-import com.baidaidai.rootless_store.domain.source.model.PluginSource
+import com.baidaidai.rootless_store.domain.source.model.PluginSourceLocal
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
@@ -9,7 +9,7 @@ import javax.inject.Inject
 class GetWholeSourceUseCase @Inject constructor(
     private val pluginSourceRepositoryImpl: PluginSourceRepositoryImpl
 ) {
-    operator fun invoke(): Flow<List<PluginSource>>{
+    operator fun invoke(): Flow<List<PluginSourceLocal>>{
         return pluginSourceRepositoryImpl.getAllPluginSources().map {
             it.orEmpty()
         }
