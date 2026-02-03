0c008b159c3d40d4bd52d235435fbbbf652c54ed
Baidaidai_GFWD <1302064396@qq.com>
2026-02-03 16:28:56 +0800

    refactor: Change usage from constructor to hilt

--- app/src/main/java/com/baidaidai/rootless_store/domain/source/usecase/AddOneSourceUseCase.kt
+++ app/src/main/java/com/baidaidai/rootless_store/domain/source/usecase/AddOneSourceUseCase.kt
@@ -4,7 +4,7 @@ import com.baidaidai.rootless_store.data.source.repository.PluginSourceRepositor
 import com.baidaidai.rootless_store.domain.source.model.PluginSourceUser
 import javax.inject.Inject
 
-class AddOneSourceUseCase(
+class AddOneSourceUseCase @Inject constructor(
     private val pluginSourceRepositoryImpl: PluginSourceRepositoryImpl
 ) {
     suspend operator fun invoke(
