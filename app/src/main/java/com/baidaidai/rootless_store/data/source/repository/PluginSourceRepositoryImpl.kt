266212e53bf37d26f00e8f6f79a884a32793bb58
Baidaidai_GFWD <1302064396@qq.com>
2026-02-03 11:20:09 +0800

    feat!: Append the logic, prevent void data

--- app/src/main/java/com/baidaidai/rootless_store/data/source/repository/PluginSourceRepositoryImpl.kt
+++ app/src/main/java/com/baidaidai/rootless_store/data/source/repository/PluginSourceRepositoryImpl.kt
@@ -3,9 +3,11 @@ package com.baidaidai.rootless_store.data.source.repository
 import android.content.Context
 import com.baidaidai.rootless_store.data.database.RootlessStoreDatabase
 import com.baidaidai.rootless_store.data.source.database.PluginSourceEntity
+import com.baidaidai.rootless_store.data.source.remote.api.PluginSourceAPI
 import com.baidaidai.rootless_store.domain.source.model.PluginSource
 import com.baidaidai.rootless_store.domain.source.repository.PluginSourceRepository
 import dagger.hilt.android.qualifiers.ApplicationContext
+import io.ktor.client.call.body
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
@@ -22,7 +24,22 @@ class PluginSourceRepositoryImpl @Inject constructor(
     override suspend fun insertOnePluginSource(
         pluginSource: PluginSource
     ) {
-        pluginSourceDAO.insertOnePluginSource(pluginSourceEntity)
+        // Check
+        if (pluginSource.sourceName.isNullOrEmpty()){
+            // fetch sourceName
+            val httpResponse = pluginSourceAPI.getPluginSourceMetaInfo(
+                pluginSourceURI = pluginSource.sourceURI
+            )
+            val httpResponseBody = httpResponse.body<PluginSource>()  // change to DTO future
+
+            val pluginSource = pluginSource.copy(sourceName = httpResponseBody.sourceName)
+            val pluginSourceEntity = PluginSourceEntity.fromPluginSource(pluginSource)
+
+            pluginSourceDAO.insertOnePluginSource(pluginSourceEntity)
+        }else{
+            val pluginSourceEntity = PluginSourceEntity.fromPluginSource(pluginSource)
+            pluginSourceDAO.insertOnePluginSource(pluginSourceEntity)
+        }
     }
 
     // Update
