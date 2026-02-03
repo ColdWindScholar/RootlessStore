79854bb9d5936322de166d0bab5f4e456dc29b4c
Baidaidai_GFWD <1302064396@qq.com>
2026-02-03 11:46:12 +0800

    feat: Add flow data support

--- app/src/main/java/com/baidaidai/rootless_store/ui/model/RootLessStoreSourcesScreenViewModel.kt
+++ app/src/main/java/com/baidaidai/rootless_store/ui/model/RootLessStoreSourcesScreenViewModel.kt
@@ -4,8 +4,11 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.cachedIn
 import com.baidaidai.rootless_store.domain.market.usecase.GetRemotePluginsUseCase
+import com.baidaidai.rootless_store.domain.plugin.model.PluginSource
 import com.baidaidai.rootless_store.domain.source.usecase.GetWholeSourceUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
@@ -14,4 +17,9 @@ class RootLessStoreSourcesScreenViewModel @Inject constructor(
     getWholeSourceUseCase: GetWholeSourceUseCase
 ): ViewModel(){
     val plugins = getRemotePluginsUseCase().cachedIn(viewModelScope)
+    val sourceList = getWholeSourceUseCase().stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5_000),
+        initialValue = emptyList<PluginSource>()
+    )
 }
