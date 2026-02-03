2c67adbadb7e1bf5f17c2e0f8b88ce76cc9d1700
Baidaidai_GFWD <1302064396@qq.com>
2026-02-03 16:19:57 +0800

    chore: Import necessary libs and remove the useless libs

--- app/src/main/java/com/baidaidai/rootless_store/ui/screens/SourcesScreen.kt
+++ app/src/main/java/com/baidaidai/rootless_store/ui/screens/SourcesScreen.kt
@@ -1,18 +1,20 @@
 package com.baidaidai.rootless_store.ui.screens
 
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.PreviewLightDark
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.paging.compose.collectAsLazyPagingItems
-import com.baidaidai.rootless_store.components.pluginsScreen.PluginInfoContainer
 import com.baidaidai.rootless_store.ui.model.RootLessStoreSourcesScreenViewModel
 
 @Composable
