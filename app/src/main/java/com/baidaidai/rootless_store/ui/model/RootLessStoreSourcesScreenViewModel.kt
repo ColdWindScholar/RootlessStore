c45d1c83106a4ee56ac3daabdecc9ac8b17e12f0
Baidaidai_GFWD <1302064396@qq.com>
2026-02-03 11:45:54 +0800

    feat: Add GetWholeSourceUseCase support

--- app/src/main/java/com/baidaidai/rootless_store/ui/model/RootLessStoreSourcesScreenViewModel.kt
+++ app/src/main/java/com/baidaidai/rootless_store/ui/model/RootLessStoreSourcesScreenViewModel.kt
@@ -4,12 +4,14 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.cachedIn
 import com.baidaidai.rootless_store.domain.market.usecase.GetRemotePluginsUseCase
+import com.baidaidai.rootless_store.domain.source.usecase.GetWholeSourceUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class RootLessStoreSourcesScreenViewModel @Inject constructor(
-    getRemotePluginsUseCase: GetRemotePluginsUseCase
+    getRemotePluginsUseCase: GetRemotePluginsUseCase,
+    getWholeSourceUseCase: GetWholeSourceUseCase
 ): ViewModel(){
     val plugins = getRemotePluginsUseCase().cachedIn(viewModelScope)
 }
