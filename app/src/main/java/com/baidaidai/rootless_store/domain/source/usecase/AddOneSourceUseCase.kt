c083de88e8c5310bebeeb03cc2097b88b154e9bc
Baidaidai_GFWD <1302064396@qq.com>
2026-02-03 11:41:03 +0800

    refactor: Change parameter type from PluginSourceEntity to PluginSource

--- app/src/main/java/com/baidaidai/rootless_store/domain/source/usecase/AddOneSourceUseCase.kt
+++ app/src/main/java/com/baidaidai/rootless_store/domain/source/usecase/AddOneSourceUseCase.kt
@@ -1,14 +1,14 @@
 package com.baidaidai.rootless_store.domain.source.usecase
 
-import com.baidaidai.rootless_store.data.source.database.PluginSourceEntity
 import com.baidaidai.rootless_store.data.source.repository.PluginSourceRepositoryImpl
+import com.baidaidai.rootless_store.domain.source.model.PluginSource
 
 class AddOneSourceUseCase(
     private val pluginSourceRepositoryImpl: PluginSourceRepositoryImpl
 ) {
     suspend operator fun invoke(
-        pluginSourceEntity: PluginSourceEntity
+        pluginSource: PluginSource
     ){
-        pluginSourceRepositoryImpl.insertOnePluginSource(pluginSourceEntity)
+        pluginSourceRepositoryImpl.insertOnePluginSource(pluginSource)
     }
 }
