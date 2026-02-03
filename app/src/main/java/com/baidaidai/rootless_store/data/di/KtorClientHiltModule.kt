6efb2403a8ef6d53dd9f26a9563b8f1ed98e8cb3
Baidaidai_GFWD <1302064396@qq.com>
2026-02-03 11:02:49 +0800

    feat: Add ktor client DI support

---
+++ app/src/main/java/com/baidaidai/rootless_store/data/di/KtorClientHiltModule.kt
@@ -0,0 +1,32 @@
+package com.baidaidai.rootless_store.data.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.android.Android
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object KtorClientHiltModule {
+    @Provides
+    @Singleton
+    fun provideKtorClient(): HttpClient {
+        return HttpClient(Android){
+            install(ContentNegotiation) {
+                json(
+                    Json {
+                        ignoreUnknownKeys = true
+                        isLenient = true
+                        explicitNulls = false
+                    }
+                )
+            }
+        }
+    }
+}
