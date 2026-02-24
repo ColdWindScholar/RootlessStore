ba862892c0a6d01d2d5f0d3670a3288b812baa06
Baidaidai_GFWD <1302064396@qq.com>
2026-02-24 16:17:22 +0800

    feat(vm & composable)!: Rename usage-mistake vm's name, Change vm use method, Add deleteButton - Rename vm usage from sourcesScreen to sourceScreen - Change vm from hitViewModel to parameter DI - Add "deleteButton" composable, filled onclick logics, placed in leadingContent

--- app/src/main/java/com/baidaidai/rootless_store/ui/screens/SourceScreen.kt
+++ app/src/main/java/com/baidaidai/rootless_store/ui/screens/SourceScreen.kt
@@ -1,27 +1,36 @@
 package com.baidaidai.rootless_store.ui.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.BadgeDefaults
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
 import com.baidaidai.rootless_store.R
 import com.baidaidai.rootless_store.ui.model.RootLessStoreSourceScreenViewModel
 import com.baidaidai.rootless_store.ui.theme.colorscheme.SourceListItemColor
@@ -29,10 +38,11 @@ import com.baidaidai.rootless_store.ui.theme.colorscheme.SourceListItemColor
 @Composable
 fun SourceScreen(
     contentPadding: PaddingValues,
+    sourceScreenViewModel: RootLessStoreSourceScreenViewModel,
     onListItemClick:(pluginSourceUri: String)-> Unit
 ){
-    val sourcesScreenViewModel = hiltViewModel<RootLessStoreSourceScreenViewModel>()
-    val pluginSourceList by sourcesScreenViewModel.sourceList.collectAsState()
+    val pluginSourceList by sourceScreenViewModel.sourceList.collectAsState()
+    val deleteButtonStatus by sourceScreenViewModel.deleterShowStatus.collectAsState()
 
     Box(
         modifier = Modifier
@@ -57,7 +67,41 @@ fun SourceScreen(
                                 )
                             },
                             colors = SourceListItemColor(),
-                            contentPadding = PaddingValues(horizontal = 16.dp, vertical = 10.dp)
+                            contentPadding = PaddingValues(horizontal = 16.dp, vertical = 10.dp),
+                            leadingContent = {
+                                Row(
+                                    verticalAlignment = Alignment.CenterVertically
+                                ) {
+                                    if (deleteButtonStatus){
+                                        Row {
+                                            IconButton(
+                                                onClick = {
+                                                    sourceScreenViewModel.deleteOneSource(pluginSource)
+                                                },
+                                                colors = IconButtonDefaults.iconButtonColors(
+                                                    containerColor = BadgeDefaults.containerColor,
+                                                    contentColor = contentColorFor(BadgeDefaults.containerColor)
+                                                ),
+                                                modifier = Modifier
+                                                    .size(20.dp)
+                                            ) {
+                                                Icon(
+                                                    painter = painterResource(R.drawable.outline_close_small_24),
+                                                    contentDescription = "Delete",
+                                                )
+                                            }
+                                            Spacer(modifier = Modifier.width(12.dp))
+                                        }
+                                    }
+                                    Image(
+                                        painter = painterResource(R.drawable.ic_launcher_background),
+                                        contentDescription = "2",
+                                        modifier = Modifier
+                                            .size(40.dp)
+                                            .clip(CircleShape)
+                                    )
+                                }
+                            }
                         ) {
                             Text(pluginSource.sourceName)
                         }
