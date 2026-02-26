008fca82dc9b16bdd6a40382354eecae775d7c1f
Baidaidai_GFWD <1302064396@qq.com>
2026-02-26 21:48:17 +0800

    refactor: Migration core logic intro PluginCoreRepositoryImpl

--- app/src/main/java/com/baidaidai/rootless_store/domain/plugin/usecase/InstallOnePluginUseCase.kt
+++ app/src/main/java/com/baidaidai/rootless_store/domain/plugin/usecase/InstallOnePluginUseCase.kt
@@ -6,34 +6,7 @@ import com.baidaidai.rootless_store.domain.plugin.error.PluginError
 import javax.inject.Inject
 
 class InstallOnePluginUseCase @Inject constructor(
-    private val pluginFileSystemGatewayImpl: PluginFileSystemGatewayImpl,
     private val pluginCoreRepositoryImpl: PluginCoreRepositoryImpl
 ){
-    suspend operator fun invoke(
-        uri: Uri,
-    ): PluginError?{
-        /**
-         * 1. Install Plugin
-         * 2. Inset DB Content
-         * 3. Update VM
-         */
-        try {
-            val pluginManiFest = pluginFileSystemGatewayImpl.readPluginManifest(uri).toManifestRoom()
-            val pluginInfoEntity = PluginInfoEntity.fromManifest(pluginManiFest)
-
-            pluginFileSystemGatewayImpl.installPlugin(uri)
-            repositoryImpl.insertOnePluginInfo(pluginInfoEntity)
-
-            return null
-        }catch (error: Throwable){
-            val errorStack  = error.stackTrace.OutOfStringLike()
-
-
-            return PluginError(
-                errorMessage = error.message!!,
-                errorCause = errorStack
-            )
-        }
-
-    }
+    suspend operator fun invoke(uri: Uri): PluginError? = pluginCoreRepositoryImpl.installOnePlugin(uri)
 }
