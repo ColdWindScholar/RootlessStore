82d4a47a38b9ad89af64e8b6893942da985c81d4
Baidaidai_GFWD <1302064396@qq.com>
2026-02-26 21:40:55 +0800

    chore: Arrange methods, Add RURD annotation

--- app/src/main/java/com/baidaidai/rootless_store/domain/plugin/repository/PluginCoreRepository.kt
+++ app/src/main/java/com/baidaidai/rootless_store/domain/plugin/repository/PluginCoreRepository.kt
@@ -6,22 +6,20 @@ import com.baidaidai.rootless_store.domain.plugin.manifest.PluginManifestLocal
 import com.baidaidai.rootless_store.domain.plugin.manifest.PluginManifestRoom
 import kotlinx.coroutines.flow.Flow
 
-    // 以为DB为中心的Gateway
 interface PluginCoreRepository {
-
-    val appDatabase: RoomDatabase
-    suspend fun getOnePluginInfo(pluginID: String): PluginManifestLocal?
-
-    fun getWholePluginInfo(): Flow<List<PluginManifestRoom>?>
-
-    fun getPluginInfoCount(): Flow<Int>
-
+    // Create
     suspend fun insertOnePluginInfo(pluginInfoEntity: PluginInfoEntity)
 
-    suspend fun deleteOnePluginInfo(pluginInfoEntity: PluginInfoEntity)
+    // Read
+    suspend fun getOnePluginInfo(pluginID: String): PluginManifestLocal?
+    fun getWholePluginInfo(): Flow<List<PluginManifestRoom>?>
+    fun getPluginInfoCount(): Flow<Int>
 
+    // Update
     suspend fun enablePluginByID(pluginID: String)
-
     suspend fun disablePluginByID(pluginID: String)
+
+    // Delete
+    suspend fun deleteOnePluginInfo(pluginInfoEntity: PluginInfoEntity)
 
 }
