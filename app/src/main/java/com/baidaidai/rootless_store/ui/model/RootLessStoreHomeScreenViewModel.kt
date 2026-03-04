05b2a9fa4b5a0ce6caa7af276463824452819468
Baidaidai_GFWD <1302064396@qq.com>
2026-03-04 16:14:21 +0800

    feat: Delete old method, use kotlin flow for best practice

--- app/src/main/java/com/baidaidai/rootless_store/ui/model/RootLessStoreHomeScreenViewModel.kt
+++ app/src/main/java/com/baidaidai/rootless_store/ui/model/RootLessStoreHomeScreenViewModel.kt
@@ -1,96 +1,32 @@
 package com.baidaidai.rootless_store.ui.model
 
-import android.content.Context
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.baidaidai.rootless_store.data.status.gateway.RAMStatusGatewayImpl
-import com.baidaidai.rootless_store.data.status.gateway.StorageStatusGatewayImpl
-import com.baidaidai.rootless_store.domain.status.model.RAMStatus
+import com.baidaidai.rootless_store.domain.status.model.MemoryStatus
 import com.baidaidai.rootless_store.domain.status.model.StorageStatus
+import com.baidaidai.rootless_store.domain.status.usecase.GetMemoryStatusUseCase
+import com.baidaidai.rootless_store.domain.status.usecase.GetStorageStatusUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import dagger.hilt.android.qualifiers.ApplicationContext
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
 class RootLessStoreHomeScreenViewModel @Inject constructor(
-    @ApplicationContext private val context: Context
+    getStorageStatusUseCase: GetStorageStatusUseCase,
+    getMemoryStatusUseCase: GetMemoryStatusUseCase
 ): ViewModel() {
-//    private var context: Context? = null
 
-    private val _storageStatus: MutableStateFlow<StorageStatus> = MutableStateFlow(getStorageStatus())
-    private val _ramStatus: MutableStateFlow<RAMStatus> = MutableStateFlow(getRAMStatus())
-    val storageStatus: StateFlow<StorageStatus> = _storageStatus.asStateFlow()
-    val ramStatus: StateFlow<RAMStatus> = _ramStatus.asStateFlow()
-
-    init {
-        keepRamReaderRunning()
-    }
-
-//    fun prepareViewModel(
-//        context: Context? = null
-//    ){
-//        if (context != null){
-//            this.context = context
-//            _upgradeThe_storageStatus()
-//            _upgradeThe_RAMStatus()
-//            keepRamReaderRunning()
-//        }
-//    }
-
-    private fun getStorageStatus(): StorageStatus{
-        return StorageStatusGatewayImpl(this.context).getStorageStatus()
-//        if (this.context != null){
-//            return
-//        }else{
-//            return StorageStatus(
-//                totalStorage = 128.0,
-//                usedStorage = 100.0
-//            )
-//        }
-    }
-    private fun getRAMStatus(): RAMStatus{
-
-        return RAMStatusGatewayImpl(this.context).getRAMStatus()
-//        if (this.context != null){
-//        }else{
-//            return RAMStatus(
-//                totalRAM = 24.0,
-//                usedRAM = 0.0
-//            )
-//        }
-    }
-    private fun _upgradeThe_storageStatus(){
-        this._storageStatus.update {
-            getStorageStatus()
-        }
-    }
-    private fun _upgradeThe_RAMStatus(){
-        this._ramStatus.update {
-            getRAMStatus()
-        }
-    }
-
-    object dbOperator{
-        private fun _appendPluginInformationsIntroDatabase(){
-
-        }
-    }
-
-    private fun keepRamReaderRunning(){
-        viewModelScope.launch {
-            while (true){
-                _upgradeThe_RAMStatus()
-                delay(1000)
-                Log.d("RAM_Status","updated, current:${getRAMStatus().usedRAM}, total${getRAMStatus().totalRAM}, precentage:${(getRAMStatus().usedRAM / getRAMStatus().totalRAM*100).toFloat()}")
-            }
-        }
-    }
+    val storageStatus: StateFlow<StorageStatus> = getStorageStatusUseCase().stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(1000),
+        initialValue = StorageStatus()
+    )
+    val memoryStatus: StateFlow<MemoryStatus> = getMemoryStatusUseCase().stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(1000),
+        initialValue = MemoryStatus()
+    )
 
 }
