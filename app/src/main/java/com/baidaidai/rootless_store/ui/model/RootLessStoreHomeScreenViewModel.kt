75b40e254447afbef617a5fc40e183e52baf3d3b
Baidaidai_GFWD <1302064396@qq.com>
2026-03-06 14:24:42 +0800

    refactor: Tidy the vm property

--- app/src/main/java/com/baidaidai/rootless_store/ui/model/RootLessStoreHomeScreenViewModel.kt
+++ app/src/main/java/com/baidaidai/rootless_store/ui/model/RootLessStoreHomeScreenViewModel.kt
@@ -2,10 +2,8 @@ package com.baidaidai.rootless_store.ui.model
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.baidaidai.rootless_store.domain.status.model.MemoryStatus
-import com.baidaidai.rootless_store.domain.status.model.StorageStatus
-import com.baidaidai.rootless_store.domain.status.usecase.GetMemoryStatusUseCase
-import com.baidaidai.rootless_store.domain.status.usecase.GetStorageStatusUseCase
+import com.baidaidai.rootless_store.domain.status.model.RootlessStoreHosterStatus
+import com.baidaidai.rootless_store.domain.status.usecase.GetRootlessStoreHosterStatusUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
@@ -14,19 +12,13 @@ import javax.inject.Inject
 
 @HiltViewModel
 class RootLessStoreHomeScreenViewModel @Inject constructor(
-    getStorageStatusUseCase: GetStorageStatusUseCase,
-    getMemoryStatusUseCase: GetMemoryStatusUseCase
+    getRootlessStoreHosterStatusUseCase: GetRootlessStoreHosterStatusUseCase
 ): ViewModel() {
 
-    val storageStatus: StateFlow<StorageStatus> = getStorageStatusUseCase().stateIn(
-        scope = viewModelScope,
-        started = SharingStarted.WhileSubscribed(1000),
-        initialValue = StorageStatus()
-    )
-    val memoryStatus: StateFlow<MemoryStatus> = getMemoryStatusUseCase().stateIn(
-        scope = viewModelScope,
-        started = SharingStarted.WhileSubscribed(1000),
-        initialValue = MemoryStatus()
-    )
-
+    val rootlessStoreHosterStatus: StateFlow<RootlessStoreHosterStatus> =
+        getRootlessStoreHosterStatusUseCase().stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(1000),
+            initialValue = RootlessStoreHosterStatus()
+        )
 }
