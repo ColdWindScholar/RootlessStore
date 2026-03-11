9e6d77a35837a9ef2887a7cef15d48b286c9a8a3
Baidaidai_GFWD <1302064396@qq.com>
2026-03-11 11:46:10 +0800

    refactor!: Add the environment checker, Change from libsu to processBuilder, Add aboutPluginProcess method

--- app/src/main/java/com/baidaidai/rootless_store/data/execute/gateway/PluginExecuteGatewayImpl.kt
+++ app/src/main/java/com/baidaidai/rootless_store/data/execute/gateway/PluginExecuteGatewayImpl.kt
@@ -2,9 +2,12 @@ package com.baidaidai.rootless_store.data.execute.gateway
 
 import com.topjohnwu.superuser.CallbackList
 import com.topjohnwu.superuser.Shell
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class PluginExecuteGatewayImpl @Inject constructor(
@@ -17,15 +20,40 @@ class PluginExecuteGatewayImpl @Inject constructor(
             override fun onAddElement(s: String) = onAddingElement(s)
         }
     }
-    fun executePluginEntryPoint(pluginExecuteEntryPoint: String): Flow<String> = callbackFlow {
-        val callbackList = createCallbackList { result ->
-            trySend("- $result")
-        }
-        Shell
-            .cmd("sh $pluginExecuteEntryPoint")
-            .to(callbackList)
-            .submit { close() }
 
-        awaitClose {  }
+    internal fun rootEnvironmentSwitch(): String{
+        val shell = Shell.getShell()
+        return if (shell.isRoot){
+            "su"
+        }else{
+            "sh"
+        }
+    }
+    fun executePluginEntryPoint(pluginExecuteEntryPoint: String,pluginPackageDirectory: String): Flow<String> = callbackFlow {
+        val process = ProcessBuilder(
+            rootEnvironmentSwitch(), "-c", "cd $pluginPackageDirectory ;echo PID:$$; exec $pluginExecuteEntryPoint"
+        )
+            .redirectErrorStream(true) // stderr 合并到 stdout
+            .start()
+
+        launch(Dispatchers.IO) {
+            process.inputStream.bufferedReader().useLines { lines ->
+                lines.forEach { line ->
+                    send("- $line")
+                }
+            }
+        }
+
+        awaitClose {
+        }
+    }
+        .flowOn(Dispatchers.IO)
+
+    fun abortPluginProcess(pluginProcessPID: Int?){
+        if (pluginProcessPID != null){
+            ProcessBuilder(
+                rootEnvironmentSwitch(), "-c", "kill $pluginProcessPID"
+            ).start()
+        }
     }
 }
