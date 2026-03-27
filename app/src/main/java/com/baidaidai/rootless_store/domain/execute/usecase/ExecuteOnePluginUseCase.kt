8f00af1029ece6d9bcb2ebf3a29e39455e4c0368
Baidaidai_GFWD <1302064396@qq.com>
2026-03-27 17:09:15 +0800

    feat: Add the adb status judgment, add shizuku execute ability

--- app/src/main/java/com/baidaidai/rootless_store/domain/execute/usecase/ExecuteOnePluginUseCase.kt
+++ app/src/main/java/com/baidaidai/rootless_store/domain/execute/usecase/ExecuteOnePluginUseCase.kt
@@ -1,12 +1,21 @@
 package com.baidaidai.rootless_store.domain.execute.usecase
 
 import com.baidaidai.rootless_store.data.execute.repository.PluginExecuteRepositoryImpl
+import com.baidaidai.rootless_store.data.status.repository.StoreStatusRepositoryImpl
 import com.baidaidai.rootless_store.domain.plugin.manifest.PluginManifestRoom
+import com.baidaidai.rootless_store.domain.status.model.HosterOverallStatus
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class ExecuteOnePluginUseCase @Inject constructor(
-    private val pluginExecuteRepositoryImpl: PluginExecuteRepositoryImpl
+    private val pluginExecuteRepositoryImpl: PluginExecuteRepositoryImpl,
+    val storeStatusRepositoryImpl: StoreStatusRepositoryImpl
 ) {
-    operator fun invoke(pluginManifestRoom: PluginManifestRoom): Flow<String> = pluginExecuteRepositoryImpl.executeOnePlugin(pluginManifestRoom)
+    operator fun invoke(
+        pluginManifestRoom: PluginManifestRoom
+    ): Flow<String> = if (storeStatusRepositoryImpl.getOverallStatus() == HosterOverallStatus.ADB) {
+            pluginExecuteRepositoryImpl.executeOnePluginByShizuku(pluginManifestRoom)
+        }else{
+            pluginExecuteRepositoryImpl.executeOnePlugin(pluginManifestRoom)
+        }
 }
--- app/src/main/java/com/baidaidai/rootless_store/domain/plugin/usecase/AbortPluginProcessUseCase.kt
+++ app/src/main/java/com/baidaidai/rootless_store/domain/plugin/usecase/AbortPluginProcessUseCase.kt
@@ -1,11 +1,18 @@
 package com.baidaidai.rootless_store.domain.plugin.usecase
 
 import com.baidaidai.rootless_store.data.execute.repository.PluginExecuteRepositoryImpl
+import com.baidaidai.rootless_store.data.status.repository.StoreStatusRepositoryImpl
 import com.baidaidai.rootless_store.domain.plugin.manifest.PluginManifestRoom
+import com.baidaidai.rootless_store.domain.status.model.HosterOverallStatus
 import javax.inject.Inject
 
 class AbortPluginProcessUseCase @Inject constructor(
-    private val pluginExecuteRepositoryImpl: PluginExecuteRepositoryImpl
+    private val pluginExecuteRepositoryImpl: PluginExecuteRepositoryImpl,
+    private val storeStatusRepositoryImpl: StoreStatusRepositoryImpl
 ) {
-    suspend operator fun invoke(pluginManifestRoom: PluginManifestRoom) = pluginExecuteRepositoryImpl.abortPluginProcess(pluginManifestRoom)
+    suspend operator fun invoke(pluginManifestRoom: PluginManifestRoom) = if (storeStatusRepositoryImpl.getOverallStatus() == HosterOverallStatus.ADB) {
+        pluginExecuteRepositoryImpl.abortPluginProcessByShizuku(pluginManifestRoom)
+    }else{
+        pluginExecuteRepositoryImpl.abortPluginProcess(pluginManifestRoom)
+    }
 }
